5b8ac81424b858d93731fa7f7a93e8e645e6dee3
mehmetztrk21 <mehmet.ztrk2134@gmail.com>
2025-09-15 16:19:14 +0300

    feat: implement CRUD endpoints for UsersController

--- 2-temel-mimari/src/users/users.controller.ts
+++ 2-temel-mimari/src/users/users.controller.ts
@@ -1,11 +1,32 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
 import { UsersService } from './users.service';
 
 @Controller('users')
 export class UsersController {
   constructor(private usersService: UsersService) {}
-  @Get()
+  @Get() // http://localhost:3000/users
   test() {
-    return this.usersService.testConfig();
+    this.usersService.testConfig();
+    return 'get user endpoint';
+  }
+
+  @Get(':id') // http://localhost:3000/users/1
+  getUserById(@Param('id') id: string) {
+    return this.usersService.getUserById(+id);
+  }
+
+  @Post() // http://localhost:3000/users
+  createUser() {
+    return 'create user endpoint';
+  }
+
+  @Put(':id') // http://localhost:3000/users/1
+  updateUser(@Param('id') id: string) {
+    return `update user endpoint for user ${id}`;
+  }
+
+  @Delete(':id') // http://localhost:3000/users/1
+  deleteUser(@Param('id') id: string) {
+    return `delete user endpoint for user ${id}`;
   }
 }
