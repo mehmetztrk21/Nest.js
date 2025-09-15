65837d52849cae3c9b2b0a6ade04e57dc963effa
mehmetztrk21 <mehmet.ztrk2134@gmail.com>
2025-09-15 13:13:52 +0300

    feat: resolve circular dependency in OrdersModule by using forwardRef for ProductsModule

--- 2-temel-mimari/src/orders/orders.module.ts
+++ 2-temel-mimari/src/orders/orders.module.ts
@@ -1,10 +1,11 @@
-import { Module } from '@nestjs/common';
+import { forwardRef, Module } from '@nestjs/common';
 import { ProductsModule } from 'src/products/products.module';
 import { UsersModule } from 'src/users/users.module';
 import { OrdersService } from './orders.service';
 
 @Module({
   providers: [OrdersService],
-  imports: [UsersModule, ProductsModule], //başka modülleri import ediyoruz ki içindeki servisleri kullanabilelim
+  imports: [UsersModule, forwardRef(() => ProductsModule)], //Circular dependency with ProductsModule, using forwardRef
+  exports: [OrdersService], //Exporting OrdersService to be used by other modules
 })
 export class OrdersModule {}
