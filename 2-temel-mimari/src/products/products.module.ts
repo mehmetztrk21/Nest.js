8d5bb4d640419e61eafdaadcc8519a35cf8d691c
mehmetztrk21 <mehmet.ztrk2134@gmail.com>
2025-09-15 12:41:05 +0300

    feat: add forwardRef to ProductsModule for circular dependency resolution with OrdersModule

--- 2-temel-mimari/src/products/products.module.ts
+++ 2-temel-mimari/src/products/products.module.ts
@@ -1,7 +1,9 @@
-import { Module } from '@nestjs/common';
+import { forwardRef, Module } from '@nestjs/common';
 import { ProductsService } from './products.service';
+import { OrdersModule } from 'src/orders/orders.module';
 
 @Module({
+  imports: [forwardRef(() => OrdersModule)], //Orders module products modülünü import ediyor, döngüsel bağımlılığı önlemek için forwardRef kullanıyoruz
   providers: [ProductsService],
   exports: [ProductsService], //products servisini dışa aktarıyoruz ki başka modüllerde de kullanabilelim
 })
