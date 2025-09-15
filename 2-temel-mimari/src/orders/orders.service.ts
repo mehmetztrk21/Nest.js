f50eabe23573eb25cb12cd1bf1d1dba212e03346
mehmetztrk21 <mehmet.ztrk2134@gmail.com>
2025-09-15 12:53:51 +0300

    feat: enhance OrdersService to dynamically inject PaymentService and include payment details in order list

--- 2-temel-mimari/src/orders/orders.service.ts
+++ 2-temel-mimari/src/orders/orders.service.ts
@@ -1,21 +1,35 @@
 import { Injectable } from '@nestjs/common';
+import { ModuleRef } from '@nestjs/core';
+import { PaymentService } from 'src/payment/payment.service';
 import { ProductsService } from 'src/products/products.service';
 import { UsersService } from 'src/users/users.service';
 
 @Injectable()
 export class OrdersService {
+  private paymentService: PaymentService;
   constructor(
     private readonly usersService: UsersService,
     private readonly productsService: ProductsService,
-  ) {}
+    private readonly moduleRef: ModuleRef,
+  ) {
+    this.paymentService = this.moduleRef.get(PaymentService, { strict: false }); //orders.module.ts içinde payment modulünü import etmeden de payment servisini kullanabilmek için ModuleRef ile dinamik olarak alıyoruz.Farklı bir yaklaşım.
+  }
 
   getOrderList(userId: number) {
     const user = this.usersService.getUserById(userId);
     return {
       user,
       orders: [
-        { orderId: 1, product: this.productsService.getProductById(1) },
-        { orderId: 2, product: this.productsService.getProductById(2) },
+        {
+          orderId: 1,
+          product: this.productsService.getProductById(1),
+          payment: this.paymentService.getPaymentType('Credit Card'),
+        },
+        {
+          orderId: 2,
+          product: this.productsService.getProductById(2),
+          payment: this.paymentService.getPaymentType('PayPal'),
+        },
       ],
     };
   }
