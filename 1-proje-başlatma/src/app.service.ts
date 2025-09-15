08b707719b13869cd7d0683b9d4acaf0f3c33908
mehmetztrk21 <mehmet.ztrk2134@gmail.com>
2025-09-15 11:41:34 +0300

    Refactor AppService to implement lifecycle hooks and update .prettierrc settings

--- 1-proje-başlatma/src/app.service.ts
+++ 1-proje-başlatma/src/app.service.ts
@@ -1,7 +1,30 @@
-import { Injectable } from '@nestjs/common';
+import {
+  Injectable,
+  OnApplicationShutdown,
+  OnModuleDestroy,
+  OnModuleInit,
+} from '@nestjs/common';
 
 @Injectable()
-export class AppService {
+export class AppService
+  implements OnModuleInit, OnModuleDestroy, OnApplicationShutdown
+{
+  onModuleInit() {
+    // ilk bu çalışır.
+    console.log('Module initialized');
+  }
+  onApplicationBootstrap() {
+    // sonra bu çalışır
+    console.log('Application bootstrapped');
+  }
+  onApplicationShutdown(signal?: string) {
+    // uygulama kapanırken çalışır
+    console.log(`Application shutdown due to: ${signal}`);
+  }
+  onModuleDestroy() {
+    //module kapatılırken çalışır
+    console.log('Module is being destroyed');
+  }
   getHello(): string {
     return 'Hello World!';
   }
