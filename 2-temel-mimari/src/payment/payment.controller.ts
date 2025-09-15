187dbd175f36dcbbd26516fb72f0a9744c532d1f
mehmetztrk21 <mehmet.ztrk2134@gmail.com>
2025-09-15 17:10:31 +0300

    feat: enhance PaymentController with request logging and improved response handling

--- 2-temel-mimari/src/payment/payment.controller.ts
+++ 2-temel-mimari/src/payment/payment.controller.ts
@@ -2,20 +2,28 @@ import {
   Controller,
   Delete,
   Get,
+  HttpCode,
   Param,
   Patch,
   Post,
   Put,
+  Req,
+  Res,
 } from '@nestjs/common';
+import type { Request, Response } from 'express'; //type import ederken type anahtar kelimesini kullanıyoruz
 
 @Controller('payment')
 export class PaymentController {
   @Get()
-  getPayment() {
-    return 'This action returns all payment methods';
+  getPayment(@Req() req: Request, @Res() res: Response) {
+    console.log(req.headers, req.ip);
+    return res
+      .status(200)
+      .json({ message: 'This action returns all payment methods' });
   }
 
   @Get(':id')
+  @HttpCode(202) // bu şekilde de status kodu belirleyebiliriz
   getPaymentById(@Param('id') id: number) {
     return `This action returns payment method #${id}`;
   }
@@ -26,8 +34,17 @@ export class PaymentController {
   }
 
   @Put(':id')
-  updatePayment(@Param('id') id: number) {
-    return `This action updates payment method #${id}`;
+  updatePayment(@Param('id') id: number, @Res() res: Response) {
+    if (id == 1) {
+      return res.status(404).json({
+        statusCode: 404,
+        message: 'Payment method not found',
+      });
+    }
+    return res.status(200).json({
+      statusCode: 200,
+      message: `Payment method #${id} updated successfully`,
+    });
   }
 
   @Patch(':id')
