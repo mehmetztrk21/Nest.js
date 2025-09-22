8ae083e2f144a93644def249c1322842d17fb213
Mehmet Öztürk <mehmet.ozturk@dincerlojistik.com>
2025-09-22 13:48:10 +0300

    feat: initialize 3-http-payloads project with NestJS structure

---
+++ 3-http-payloads/src/app.controller.ts
@@ -0,0 +1,41 @@
+import { Body, Controller, Get, Param, Patch, Post, Put } from '@nestjs/common';
+import { AppService } from './app.service';
+import type { User } from './models/user.model';
+
+@Controller()
+export class AppController {
+  constructor(private readonly appService: AppService) { }
+
+  @Post()
+  createData(@Body() user: User) {
+    console.log('User data received:', user.name);
+    return {
+      message: 'User data received successfully',
+      userData: user,
+
+    }
+  }
+
+  @Put(':id')
+  updateData(@Body() user: User, @Param('id') id: string) {
+    console.log('User data updated:', user.name, id);
+    return {
+      message: 'User data updated successfully',
+      userData: user,
+    };
+  }
+
+  @Patch(':id')
+  partialUpdateData(@Body() user: Partial<User>, @Param('id') id: string) {
+    console.log('User data partially updated:', user.name, id);
+    return {
+      message: 'User data partially updated successfully',
+      userData: user,
+    };
+  }
+
+  @Get()
+  getHello(): string {
+    return this.appService.getHello();
+  }
+}
