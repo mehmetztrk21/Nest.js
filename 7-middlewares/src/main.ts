5e8c90dd3425acf72c86c9743a5936ff25398b33
Mehmet Öztürk <mehmet.ozturk@dincerlojistik.com>
2025-09-25 11:57:22 +0300

    feat: enable CORS middleware in main.ts for cross-origin requests

--- 7-middlewares/src/main.ts
+++ 7-middlewares/src/main.ts
@@ -5,6 +5,13 @@ import { LoggerMiddleware } from './logger/logger.middleware';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(new LoggerMiddleware().use); // global olarak middleware i uyguladık.
+  app.enableCors({ // CORS ayarları
+    origin: '*', // tüm domainlere izin ver
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE', // izin verilen methodlar
+    preflightContinue: false, // preflight request i devam ettirme
+    credentials: true, // cookie gönderme izni
+    allowedHeaders: 'Content-Type, Accept, Authorization', // izin verilen headerlar
+  });
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
